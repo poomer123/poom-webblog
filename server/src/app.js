43dde101bfa475d89df4b2d099fc4846d01afced
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-02-16 10:19:35 +0700

    basic resfull API server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,43 @@
-console.log('hello server edit')
+let express = require('express')
+const app = express()
+let port = 8080
+
+app.get('/status', (req, res) => {
+    res.send('hello nodeJs server')
+})
+
+// user section
+// ============
+// create
+app.post('/user', (req, res) => {
+    res.send('Ok I create user')
+})
+
+// update
+app.put('/user/:userId', (req, res) => {
+    res.send('Ok I updated user')
+})
+
+// delete
+app.delete('/user/:userId', (req, res) => {
+    res.send('Ok I delete user')
+})
+
+// view
+app.get('/users', (req, res) => {
+    res.send('view all users')
+})
+
+// view by Id
+app.get('/user/:userId', (req, res) => {
+    res.send('view user by Id')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('server is running ' + port)
+})
