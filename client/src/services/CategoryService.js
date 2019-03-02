6e7b56ab41a40c2401b585649b72b9282b293131
Apirat Chiwakanit <poom_popping@hotmail.com>
2019-03-02 15:36:14 +0700

    category service preared

--- client/src/services/CategoryService.js
+++ client/src/services/CategoryService.js
@@ -1,7 +1,31 @@
 import Api from '@/services/Api'
 
 export default {
+    // get all categories
     index() {
         return Api().get('categories')
+    },
+
+    // get by id
+    show(categoryId) {
+        return Api().get('category/'+categoryId)
+    },
+
+
+    // create category
+    create(category) {
+        return Api().post('category', category)
+    },
+
+
+    // edit
+    edit(category) {
+        return Api().put('category/'+category.id, category)
+    },
+    
+
+    // delete
+    delete(category) {
+        return Api().delete('category/'+category.id, category)
     }
 }
