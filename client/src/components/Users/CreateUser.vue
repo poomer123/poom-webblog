f786a5d797c9e7e1b6bfec2c5e6ec6b6506e4be7
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-03-02 11:44:12 +0700

    CURD user client site

--- client/src/components/Users/CreateUser.vue
+++ client/src/components/Users/CreateUser.vue
@@ -1,12 +1,42 @@
 <template>
     <div>
-        <h1>Create User</h1>
+        <h1>Create New User</h1>
+        <form v-on:submit.prevent="createUser">
+            <p>Name : <input type="text" v-model="user.name"></p>
+            <p>Lastname : <input type="text" v-model="user.lastName"></p>
+            <p>Email : <input type="email" v-model="user.email"></p>
+            <p>Password : <input type="text" v-model="user.password"></p>
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
+import UserService from '@/services/UserService'
+
 export default {
-    
+    data() {
+        return {
+            user: {
+                name: '',
+                lastName: '',
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        async createUser() {
+            try {
+                await UserService.create(this.user)
+                this.$router.push({
+                    name: 'users'
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
 }
 </script>
 
--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -1,7 +1,31 @@
 import Api from '@/services/Api'
 
 export default {
+    // get all users
     index() {
         return Api().get('users')
+    },
+
+    // get by id
+    show(userId) {
+        return Api().get('user/'+userId)
+    },
+
+
+    // create user
+    create(user) {
+        return Api().post('user', user)
+    },
+
+
+    // edit
+    edit(user) {
+        return Api().put('user/'+user.id, user)
+    },
+    
+
+    // delete
+    delete(user) {
+        return Api().delete('user/'+user.id, user)
     }
 }
